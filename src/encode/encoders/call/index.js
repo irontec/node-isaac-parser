f278d4331d09f242ce25b7342df7770ea7b6d5db
Mikel Eizagirre <meizagirre@irontec.com>
2017-05-17 08:58:17 +0200

    Added redirect method

--- src/encode/encoders/call/index.js
+++ src/encode/encoders/call/index.js
@@ -5,13 +5,15 @@ import answer from './answer';
 import hangup from './hangup';
 import hold from './hold';
 import unhold from './unhold';
+import redirect from './redirect';
 
 var call = {
   create,
   answer,
   hangup,
   hold,
-  unhold
+  unhold,
+  redirect
 };
 
 export default call;
---
+++ src/encode/encoders/call/redirect.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import debug from 'debug';
+import isUndefined from 'lodash.isundefined';
+
+const log = debug('isaac-wrapper.parser:err');
+
+export default function getRedirectMessage(refId, extension) {
+
+  if (isUndefined(refId)) {
+    log('Missing parameter: refId on encoder.call.getRedirectMessage');
+    throw new Error('Missing parameter: refId on encoder.call.getRedirectMessage');
+  }
+
+  if (isUndefined(extension)) {
+    log('Missing parameter: extension on encoder.call.getRedirectMessage');
+    throw new Error('Missing parameter: extension on encoder.call.getRedirectMessage');
+  }
+
+  return ['REDIRECTUID', refId, 'agentes', extension].join(' ');
+}
